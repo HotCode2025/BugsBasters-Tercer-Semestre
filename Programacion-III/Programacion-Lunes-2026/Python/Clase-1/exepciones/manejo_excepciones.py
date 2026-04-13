890637ce005b937190975f0aa515308d81f26418
Brian Sastre <briansastre159@gmail.com>
2026-04-13 19:47:26 -0300

    Clase 1 Parte 4

--- Programacion-III/Programacion-Lunes-2026/Python/Clase-1/exepciones/manejo_excepciones.py
+++ Programacion-III/Programacion-Lunes-2026/Python/Clase-1/exepciones/manejo_excepciones.py
@@ -1,12 +1,14 @@
 resultado = None
-a=10
-b=0
+a = 7
+b = 0
 
 try:
-    resultado = a/b
+    resultado = a / b # modificamos
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
 except ZeroDivisionError as e:
-    print(f'Ocurrio un error:{e}')
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
 
-    
 print(f'El resultado es: {resultado}')
-print('Seguimos..')
