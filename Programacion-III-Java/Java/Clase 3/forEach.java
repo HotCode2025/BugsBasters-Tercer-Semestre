11e4ee6777c80323ce03117f8b8a60abbb2b344c
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-04-27 13:40:07 -0300

    creo el archivo forEach

---
+++ Programacion-III-Java/Java/Clase 3/forEach.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestForEach {
+    public static avoid main(String [] args) {
+        int edades[] = {5, 6, 8, 9}; //sintaxis resumida
+        for (int edad: edades) {
+            System.out.PrintIn("edad = " + edad);
+        }
+    }
+}
