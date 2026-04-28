c6663b9c8a44f1654d1f8c538beda0920a4c7384
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-04-28 11:25:38 -0300

    finalizo la sección Autoboxing Unboxing

--- Programacion-III-Java/Java/Clase 3/TestAutoboxingUnboxing.java
+++ Programacion-III-Java/Java/Clase 3/TestAutoboxingUnboxing.java
@@ -1,16 +1,27 @@
 package test;
 
-public class TestForEach {
-    public static avoid main(String [] args) {
-        int edades[] = {5, 6, 8, 9}; //sintaxis resumida
-        for (int edad: edades) {
-            System.out.PrintIn("edad = " + edad);
-        }
-        Persona personas[] = {new Persona ('Juan'), new Persona('Carla'), new Persona('Beatriz')};
+public class TestAutoboxingUnboxing {
+    public static void main(String [] args) {
+        //Clases envolventes o Wrapper
+        /*
+            Clases envolventes de tipos primitivos
 
-        //ForEach
-        for(Persona persona: personas){
-            System.out.PrintIn("persona = " + persona);
-        }
+            int = la clase envolventes es Integer
+            long = la clase envolvente es Long
+            float = la clase envolvente es Float
+            double = la clase envolvente es Double
+            boolean = la clase envolvente es Boolean
+            byte = la clase envolvente es Byte
+            char = la clase envolvente es Char
+            short = la clase envolvente es Short
+        */
+
+       int enteroPrim = 10; //Tipo Primitivo 
+       System.out.PrintIn("enteroPrim = " + enteroPrim);
+       Integer entero = 10; //Tipo object con la clase Integer
+       System.out.PrintIn("entero = " + entero.doubleValue());  //Autoboxing
+
+       int entero2 = entero; //Unboxing
+       System.out.PrintIn("entero2 = " + entero2);
     }
 }
