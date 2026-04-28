76568b6da37a05af66985b3acc52f9bd8f1ca65d
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-04-28 15:57:44 -0300

    termine modificadores de acceso public

--- Programacion-III-Java/Java/Clase 3/TestModificadoresAcceso.java
+++ Programacion-III-Java/Java/Clase 3/TestModificadoresAcceso.java
@@ -1,5 +1,10 @@
 package test;
+import paquete1.Clase1;
 
 public class TestModificadoresAcceso {
-
+    public static void main (String [] args) {
+        Clase1 clase1 = new clase1();
+        System.out.PrintIn("clase1 = " + clase1.atributoPublic);
+        clase1.metodoPublico();
+    }
 }
