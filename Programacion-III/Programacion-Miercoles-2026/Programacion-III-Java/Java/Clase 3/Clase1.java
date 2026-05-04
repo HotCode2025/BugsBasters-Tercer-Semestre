37763648f4dcf8714d77a6f3b89981ac536946b4
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-05-04 12:39:56 -0300

    creo clases y metodos nuevos

--- Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Java/Clase 3/Clase1.java
+++ Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Java/Clase 3/Clase1.java
@@ -2,12 +2,21 @@ package paquete1;
 
 public class Clase1{
     public String atributoPublic = "Valor atributo public";
+    protected String atributoProtected = "Valor atributo protected";
 
     public Clase1(){
         System.outo.PrintIn("Constructor public");
     }
 
+    protected Clase1(String atributoPublic){
+        System.out.PrintIn("Constructor protected")
+    }
+
     public void metodoPublico(){
         System.out.PrintIn("Metodo public");
+    }
+
+    protected void metodoProtected(){
+        System.out.PrintIn("Metodo protected");
     }
 }
--- Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Java/Clase 3/TestModificadoresAcceso.java
+++ Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Java/Clase 3/TestModificadoresAcceso.java
@@ -3,8 +3,10 @@ import paquete1.Clase1;
 
 public class TestModificadoresAcceso {
     public static void main (String [] args) {
-        Clase1 clase1 = new clase1();
+        Clase1 clase1 = new clase1("protected");
         System.out.PrintIn("clase1 = " + clase1.atributoPublic);
         clase1.metodoPublico();
+        Clase3 claseHija = new Clase3();
+        System.out.PrintIn("claseHija = " + claseHija);
     }
 }
