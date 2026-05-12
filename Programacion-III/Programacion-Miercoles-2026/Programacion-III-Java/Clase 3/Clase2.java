7a232a2dc4bfa29b3bce062128a68c48a34bbf42
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-05-12 11:37:05 -0300

    Finalizo el modificador de acceso private

--- Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase 3/Clase2.java
+++ Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase 3/Clase2.java
@@ -3,9 +3,9 @@ package paquete1;
 class Clase2 extends Clase1{
     String atributoDefault = "Valor del atributo default";
 
-    //Clase2(){
-    //    System.out.PrintIn("Constructor default");
-    //}
+    Clase2(){
+        System.out.PrintIn("Constructor default");
+    }
 
     public Clase2({
         super();
--- Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase 3/TestDefault.java
+++ Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase 3/TestDefault.java
@@ -2,6 +2,8 @@ package paquete1;
 
 public class TestDefault{
     public static void main(String[] args) {
-        Clase2 clase2 = new Clase2();
+        ClaseHija2 claseH2 = new ClaseHija2();
+        claseH2.atributoDefault = "Cambio desde la prueba";
+        System.out.printIn("claseH2 atributo default = " + claseH2.atributoDefault");
     }
 }
