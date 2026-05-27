8575d82d31874b8416d3ab76b7e3ee4eb9c03dd6
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-05-27 15:53:49 -0300

    Comienzo la clase 5

---
+++ Programacion-III/Programacion-Lunes-2026/JavaScript/Clase-5/funcionesFlecha.js
@@ -0,0 +1,18 @@
+
+
+function miFuncion(){
+    console.log('Saludos desde mi funcion')
+}
+miFuncion();
+
+let myFuncion = function (){
+    console.log('Saludos desde la funcion anonima')
+}
+
+//Ahora vamos a crear una funcion flecha
+let miFuncionFlecha = () => {
+    console.log('Saludos desde mi funcion flecha');
+}
+
+//Hay mas variantes para la funcion flecha
+miFuncionFlecha();
