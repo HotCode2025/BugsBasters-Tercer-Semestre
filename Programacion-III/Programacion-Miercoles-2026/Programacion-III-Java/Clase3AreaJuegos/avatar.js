64d8f5440c74aa856c90fe1379a538758673b346
Candelaria Ribotta <candelariaribotta385@gmail.com>
2026-05-27 15:11:41 -0300

    Update avatar.js

--- Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase3AreaJuegos/avatar.js
+++ Programacion-III/Programacion-Miercoles-2026/Programacion-III-Java/Clase3AreaJuegos/avatar.js
@@ -1,1 +1,22 @@
+function seleccionarPersonajeJugador() {
 
+    let inputZuko = document.getElementById('zuko')
+    let inputKatara = document.getElementById('katara')
+    let inputAang = document.getElementById('aang')
+    let inputToph = document.getElementById('toph')
+
+    if (inputZuko.checked) {
+        alert('Seleccionaste a ZUKO 🔥')
+    } else if (inputKatara.checked) {
+        alert('Seleccionaste a KATARA 💧')
+    } else if (inputAang.checked) {
+        alert('Seleccionaste a AANG 🌪️')
+    } else if (inputToph.checked) {
+        alert('Seleccionaste a TOPH 🌱')
+    } else {
+        alert('DEBES SELECCIONAR UN PERSONAJE')
+    }
+}
+
+let botonPersonajeJugador = document.getElementById('boton-personaje');
+botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
