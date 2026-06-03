31c5d766b7a0f21a1824eeb8a893286c28cbe5f9
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2026-06-03 14:34:50 -0300

    Avanzo con la clase

--- Programacion-III/Programacion-Lunes-2026/JavaScript/Clase-5/funcionesFlecha.js
+++ Programacion-III/Programacion-Lunes-2026/JavaScript/Clase-5/funcionesFlecha.js
@@ -16,3 +16,20 @@ let miFuncionFlecha = () => {
 
 //Hay mas variantes para la funcion flecha
 miFuncionFlecha();
+
+//Lo hacemos en una linea
+const saludar = () => console.log('Saludo a todos desde esta funcion flecha')
+console.log(saludar);
+
+
+//Otro ejemplo
+const saludar2 = () => {
+    return 'Saludo a todos desde esta funcion flecha 2'
+}
+
+console.log(saludar2);
+
+//Simplificamos la funcion anterior
+const saludar3 = () => 'Saludos desde la funcion flecha tres'
+
+console.log(saludar3);
