131ba332491210cceeebccc3262321a0e2252708
Brian Sastre <briansastre159@gmail.com>
2026-06-03 22:02:25 -0300

    finalizado el avatar js como se explican en los videos de la clase

--- Programacion-III/Programacion-Lunes-2026/JavaScript/Practicas/Clase3AreaJuegos/avatar.js
+++ Programacion-III/Programacion-Lunes-2026/JavaScript/Practicas/Clase3AreaJuegos/avatar.js
@@ -1,3 +1,6 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function seleccionarPersonajeJugador() {
     let inputZuko = document.getElementById('zuko')
     let inputKatara = document.getElementById('katara')
@@ -41,5 +44,50 @@ function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function ataquePunio() {
+    ataqueJugador = 'PUÑO'
+    alert(ataqueJugador)
+    seleccionarAtaqueEnemigo() // <-- Llamamos al nuevo nombre de la función
+}
+
+function ataquePatada() {
+    ataqueJugador = 'PATADA'
+    alert(ataqueJugador)
+    seleccionarAtaqueEnemigo() // <-- Llamamos al nuevo nombre de la función
+}
+
+function ataqueBarrida() {
+    ataqueJugador = 'BARRIDA'
+    alert(ataqueJugador)
+    seleccionarAtaqueEnemigo() // <-- Llamamos al nuevo nombre de la función
+}
+
+// Le cambiamos el nombre a la función para que no choque con la variable
+function seleccionarAtaqueEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3)
+    if(ataqueAleatorio == 1) {
+        ataqueEnemigo = 'PUÑO' // Aquí guardamos el texto en la variable
+    } else if(ataqueAleatorio == 2) {
+        ataqueEnemigo = 'PATADA'
+    } else {
+        ataqueEnemigo = 'BARRIDA'
+    }
+    // Opcional: Puedes agregar un alert aquí para ver qué eligió el enemigo y comprobar que funciona
+    // alert("El enemigo atacó con: " + ataqueEnemigo)
+}
+
+// --- ESCUCHADORES DE EVENTOS (EVENT LISTENERS) ---
+
+// Botón de seleccionar personaje
 let botonPersonajeJugador = document.getElementById('boton-personaje');
 botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+
+// Botones de ataques físicos
+let botonPunio = document.getElementById('boton-punio') 
+botonPunio.addEventListener('click', ataquePunio)
+
+let botonPatada = document.getElementById('boton-patada')
+botonPatada.addEventListener('click', ataquePatada)
+
+let botonBarrida = document.getElementById('boton-barrida')
+botonBarrida.addEventListener('click', ataqueBarrida);
